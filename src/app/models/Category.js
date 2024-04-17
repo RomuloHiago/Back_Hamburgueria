3cc54aa6cbb3f4c1bc7d9d6fc48c7bb1fb0939c0
Romulo Domingues <romulo.domingues@qsdobrasil.com>
2024-04-17 16:32:25 -0300

    ajustes na minha rota de api

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -9,7 +9,7 @@ class Category extends Model {
                 url: {
                     type: Sequelize.VIRTUAL,
                     get() {
-                        return `http://localhost:3001/category-file/${this.path}`
+                        return `https://backhamburgueria-production.up.railway.app/category-file/${this.path}`
                     },
                 },
             },
